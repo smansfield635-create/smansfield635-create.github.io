b3ff56f9745a8faef243b8263a8365911efa2e66
smansfield635-create <smansfield635@gmail.com>
2026-01-29 17:31:18 -0600

    Create cfwa.js

---
+++ assets/cfwa.js
@@ -0,0 +1,8 @@
+// Cloudflare Web Analytics (global live)
+(function(){
+  var s=document.createElement("script");
+  s.defer=true;
+  s.src="https://static.cloudflareinsights.com/beacon.min.js";
+  s.setAttribute("data-cf-beacon",'{"token":"PASTE_TOKEN_HERE"}');
+  document.head.appendChild(s);
+})();

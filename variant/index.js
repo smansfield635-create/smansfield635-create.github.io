fb97732c504ca646f00599ed4898f229c060fb15
smansfield635-create <smansfield635@gmail.com>
2026-03-06 00:58:39 -0600

    Update index.js

--- variant/index.js
+++ variant/index.js
@@ -1,3 +1,11 @@
-const script=document.createElement("script")
-script.src="/variant/scene.js"
-document.body.appendChild(script)
+(function(){
+  "use strict";
+
+  if (window.__DGB_VARIANT_BOOTED__) return;
+  window.__DGB_VARIANT_BOOTED__ = true;
+
+  const script = document.createElement("script");
+  script.src = "/variant/scene.js";
+  script.defer = true;
+  document.body.appendChild(script);
+})();

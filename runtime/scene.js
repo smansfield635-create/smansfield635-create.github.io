d7cb0c954a6d9d29f40396a6d34facab1ebe467a
smansfield635-create <smansfield635@gmail.com>
2026-04-04 21:06:44 -0500

    Update scene.js

--- runtime/scene.js
+++ runtime/scene.js
@@ -1,1 +1,39 @@
+DESTINATION: /runtime/scene.js
+(() => {
+  "use strict";
 
+  const NOOP = () => {};
+  const RETURN_NULL = () => null;
+  const RETURN_FALSE = () => false;
+
+  const sceneStub = {
+    __type: "SCENE_STUB",
+    __active: false,
+    __authority: "DISABLED_FOR_COMPASS_BASELINE",
+    init: NOOP,
+    boot: NOOP,
+    start: NOOP,
+    stop: NOOP,
+    destroy: NOOP,
+    render: NOOP,
+    resize: NOOP,
+    update: NOOP,
+    attach: NOOP,
+    detach: NOOP,
+    mount: NOOP,
+    unmount: NOOP,
+    setCanvas: NOOP,
+    getCanvas: RETURN_NULL,
+    getState: () => ({
+      active: false,
+      authority: "DISABLED_FOR_COMPASS_BASELINE"
+    }),
+    isActive: RETURN_FALSE
+  };
+
+  if (typeof window !== "undefined") {
+    window.scene = sceneStub;
+    window.SceneRuntime = sceneStub;
+    window.__RUNTIME_SCENE_DISABLED__ = true;
+  }
+})();

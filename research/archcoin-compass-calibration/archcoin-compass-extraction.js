90c7cb542ad5bcebcff0ae64a9799d27ba0a2dab
smansfield635-create <smansfield635@gmail.com>
2026-07-22 21:43:59 -0500

    Add ARCHCOIN Compass extraction artifact

---
+++ research/archcoin-compass-calibration/archcoin-compass-extraction.js
@@ -0,0 +1,25 @@
+export const ARCHCOIN_COMPASS_EXTRACTION = Object.freeze({
+  schema: "ARCHCOIN_COMPASS_EXTRACTION_v1",
+  status: "EXTRACTION_PENDING",
+  sourceFamily: "ARCHCOIN_SOURCE_FAMILY",
+  sourceRoots: Object.freeze(["/products/archcoin/"]),
+  targetCapabilities: Object.freeze([
+    "LITERAL_CARDINAL_SEMANTICS",
+    "CURRENT_CLUSTER_BEHAVIOR",
+    "CENTER_AUTHORITY",
+    "LABELS",
+    "FIXED_AXIS_DEFICIENCIES",
+    "SELECTION_PROBLEMS",
+    "PRESERVED_SUCCESSFUL_BEHAVIOR"
+  ]),
+  preservedCandidateInputs: Object.freeze([
+    "RESTORED_FUNCTIONAL_ARCHCOIN_BASELINE",
+    "TESTED_INDEX_PLANET_CANDIDATE"
+  ]),
+  evidence: Object.freeze([]),
+  extractedCapabilities: Object.freeze([]),
+  deficiencies: Object.freeze([]),
+  preservedBehavior: Object.freeze([]),
+  admissionState: "NOT_EVALUATED",
+  liveProductMutationAuthorized: false
+});

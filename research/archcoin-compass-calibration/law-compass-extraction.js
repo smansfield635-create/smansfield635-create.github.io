62343cb4f6135323596b999c7d861d87aa56b294
smansfield635-create <smansfield635@gmail.com>
2026-07-22 21:43:38 -0500

    Add Law Compass extraction artifact

---
+++ research/archcoin-compass-calibration/law-compass-extraction.js
@@ -0,0 +1,18 @@
+export const ARCHCOIN_LAW_COMPASS_EXTRACTION = Object.freeze({
+  schema: "ARCHCOIN_LAW_COMPASS_EXTRACTION_v1",
+  status: "EXTRACTION_PENDING",
+  sourceFamily: "LAW_COMPASS_SOURCE_FAMILY",
+  sourceRoots: Object.freeze(["/laws/"]),
+  targetCapabilities: Object.freeze([
+    "FLUID_MOTION",
+    "INTERACTION_SENSITIVITY",
+    "INTERPOLATION",
+    "HIT_CORRIDORS",
+    "RESPONSIBILITY_SEPARATION"
+  ]),
+  evidence: Object.freeze([]),
+  extractedCapabilities: Object.freeze([]),
+  conflictsAndLimits: Object.freeze([]),
+  admissionState: "NOT_EVALUATED",
+  productionAuthority: false
+});

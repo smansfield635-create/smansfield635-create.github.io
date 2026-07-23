24fe10e519c863d2379e4460dffdc8fdf3716e7e
smansfield635-create <smansfield635@gmail.com>
2026-07-22 21:45:09 -0500

    Add ARCHCOIN compatibility profile

---
+++ research/archcoin-compass-calibration/archcoin-compatibility-profile.js
@@ -0,0 +1,35 @@
+export const ARCHCOIN_COMPATIBILITY_PROFILE = Object.freeze({
+  schema: "ARCHCOIN_COMPATIBILITY_PROFILE_v1",
+  status: "PROFILE_INCOMPLETE",
+  sourceBaseline: "RESTORED_FUNCTIONAL_ARCHCOIN_BASELINE",
+  requiredIdentity: Object.freeze([
+    "LITERAL_CARDINAL_SEMANTICS",
+    "FINANCIAL_DOMAIN_NAVIGATION",
+    "CENTER_GLOBE",
+    "ARCHCOIN_NAMES_ROUTES_COPY_COLORS_AND_ROOM_RECORDS"
+  ]),
+  requiredCalibrationOutcomes: Object.freeze([
+    "FIXED_AXIS_CLUSTER_DISCIPLINE",
+    "STABLE_SELECTION",
+    "DETERMINISTIC_RETURN",
+    "COHERENT_SHARED_CLUSTER_RELATION",
+    "VISUAL_SEMANTIC_PROJECTION_ALIGNMENT",
+    "INTERRUPTION_SAFE_GESTURES",
+    "USABLE_REDUCED_MOTION"
+  ]),
+  preservedSuccessfulBehavior: Object.freeze([]),
+  admittedUniversalInvariants: Object.freeze([]),
+  admittedCalibrationParameters: Object.freeze([]),
+  admittedOptionalCapabilities: Object.freeze([]),
+  rejectedMechanisms: Object.freeze([]),
+  unresolvedConflicts: Object.freeze([]),
+  applicationGate: Object.freeze({
+    allFourSourceFamiliesExtracted: false,
+    crossCompassMatrixComplete: false,
+    conflictLedgerComplete: false,
+    universalStandardCandidateComplete: false,
+    compatibilityProfileComplete: false,
+    admissionDecisionsExplicit: false,
+    liveArchcoinMutationAuthorized: false
+  })
+});

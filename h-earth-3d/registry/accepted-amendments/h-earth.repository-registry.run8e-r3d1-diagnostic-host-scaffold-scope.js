4cd531c0aa70b5734ebfe9de3a5a3d19f412c47f
smansfield635-create <smansfield635@gmail.com>
2026-07-27 13:45:00 -0500

    Record R3D1 core scaffold execution

--- h-earth-3d/registry/accepted-amendments/h-earth.repository-registry.run8e-r3d1-diagnostic-host-scaffold-scope.js
+++ h-earth-3d/registry/accepted-amendments/h-earth.repository-registry.run8e-r3d1-diagnostic-host-scaffold-scope.js
@@ -35,17 +35,19 @@ const OCCURRENCES = Object.freeze(H_EARTH_RUN_8E_R3D1_PATHS.map((repositoryPath)
   gitBlobSha: null,
   contentSha256: null,
   byteCount: null,
-  existenceStatus: repositoryPath === PASS_RECEIPT_PATH ? 'RESERVED_UNTIL_PASS_CLOSED' : 'PRESENT_OR_PLANNED',
-  fetchbackStatus: 'R3D1_SCAFFOLD_VALIDATION_AND_DURABLE_RECEIPT_PENDING',
-  occurrenceClass: 'RUN_8E_R3D1_DIAGNOSTIC_HOST_SCAFFOLD_OCCURRENCE'
+  existenceStatus: repositoryPath === PASS_RECEIPT_PATH ? 'RESERVED_UNTIL_PASS_CLOSED' : 'PRESENT',
+  fetchbackStatus: repositoryPath === PASS_RECEIPT_PATH
+    ? 'R3D1_CORE_EXECUTION_PASS_DURABLE_RECEIPT_PENDING'
+    : 'R3D1_CORE_EXECUTION_PASS_FETCHBACK_ESTABLISHED',
+  occurrenceClass: 'RUN_8E_R3D1_DIAGNOSTIC_HOST_SCAFFOLD_CORE_PASS_OCCURRENCE'
 })));
 
 export const H_EARTH_RUN_8E_R3D1_EVIDENCE = freeze({
   evidenceId: 'EVIDENCE_H_EARTH_RUN_8E_R3D1_DIAGNOSTIC_HOST_SCAFFOLD_v1',
-  evidenceClass: 'R3D1_DIAGNOSTIC_DIRECTORY_HOST_AND_PLACEHOLDER_PATH_VALIDATION_PENDING',
-  sourceKind: 'GITHUB_ACTIONS_STATIC_PATH_MODULE_AND_HTTP_RESOLUTION',
+  evidenceClass: 'EXECUTED_R3D1_DIAGNOSTIC_DIRECTORY_HOST_MODULE_AND_STATIC_HTTP_PATH_VALIDATION',
+  sourceKind: 'GITHUB_ACTIONS_STATIC_PATH_MODULE_HTTP_RESOLUTION_AND_REGISTRY_PREFLIGHT_ARTIFACT',
   sourceIdOrPath: '/showroom/globe/h-earth/diagnostic/run8e-r3d/',
-  sourceOccurrenceOrRevision: null,
+  sourceOccurrenceOrRevision: '7cff5f1800c6e0743a44ac41ed501bf0c266dc61',
   assertionScope: [
     'DIAGNOSTIC_DIRECTORY_CREATED',
     'HTML_HOST_CREATED',
@@ -54,20 +56,35 @@ export const H_EARTH_RUN_8E_R3D1_EVIDENCE = freeze({
     'LIVE_GPU_BINDING_PLACEHOLDER_CREATED',
     'RELATIVE_MODULE_PATHS_RESOLVE',
     'STATIC_HTTP_PATHS_RESOLVE',
+    'ELEVEN_REGISTRY_PATHS_RESOLVE',
     'NO_INTERACTION_OR_RENDERER_EXECUTION',
+    'R3D1_CORE_EXECUTION_PASS',
     'STOP_BEFORE_R3D2'
   ],
-  verifiedOn: null,
+  verifiedOn: '2026-07-27',
   evidenceMetadata: {
     baseExactHead: '5c7a7eef489da94a230812eecc5e531e285b7cac',
+    pullRequest: 234,
     predecessorPullRequest: 231,
-    requiredDiagnosticPathCount: 4,
+    successfulExecutionHead: '7cff5f1800c6e0743a44ac41ed501bf0c266dc61',
+    workflowRun: 30294915207,
+    workflowJob: 90073456239,
+    artifactId: 8664228635,
+    artifactDigest: 'sha256:c650f1f3c391079f7cfaf564ea447687cf7afdc798e515a9e3fdb9d9e25f23fc',
+    automaticRegistryPreflightRun: 30294915184,
+    automaticRegistryPreflight: 'PASS',
+    diagnosticPathCount: 4,
+    registeredPathCount: 11,
+    staticHttpPathCount: 4,
+    executionBoundaryViolationCount: 0,
     protectedPersistentRendererGitBlob: 'b8b3c713d5f0b7c79808e8942ce385887589d880',
+    protectedNavigationSourceGitBlob: '8ab3446c536fc24423d5601acce232b19fa71c91',
     protectedPublicRouteGitBlob: 'b5f72fb70f59276f868a5894ee0c5e8beccc40ca',
     protectedPublicDirectManipulationGitBlob: '322ee2bfed5184acd8eac600f19abd72380b6c2b'
   },
   evidenceLimitations: [
-    'SCAFFOLD_VALIDATION_PENDING',
+    'DURABLE_R3D1_PASS_RECEIPT_PENDING',
+    'FINAL_EXACT_HEAD_REVALIDATION_PENDING',
     'NO_POINTER_TOUCH_OR_WHEEL_BINDING',
     'NO_NAVIGATION_PROPOSAL_EXECUTION',
     'NO_WEBGL_OR_PERSISTENT_RENDERER_EXECUTION',
@@ -88,10 +105,10 @@ export const H_EARTH_RUN_8E_R3D1_NODE = freeze({
   repositoryOccurrences: OCCURRENCES,
   evidenceClass: H_EARTH_RUN_8E_R3D1_EVIDENCE.evidenceClass,
   evidenceReferences: [H_EARTH_RUN_8E_R3D1_EVIDENCE.evidenceId],
-  authorityClass: 'BOUNDED_DIAGNOSTIC_HOST_SCAFFOLD_EXECUTION_PENDING',
-  authorityPosture: 'R3D1_EXECUTION_PENDING_UNMERGED_R3D2_NOT_STARTED_RUN_8E_FAIL_OPEN',
-  authoritySource: ['R3C_PASS_CLOSED_PERSISTENT_RENDERER', 'R3D_BOUNDED_SUBCHECKPOINT_DECOMPOSITION'],
-  authorityScope: ['CREATE_DIAGNOSTIC_DIRECTORY', 'CREATE_HTML_HOST', 'CREATE_NONEXECUTING_PLACEHOLDERS', 'VALIDATE_PATH_RESOLUTION'],
+  authorityClass: 'EXECUTED_BOUNDED_DIAGNOSTIC_HOST_SCAFFOLD_CORE_PASS',
+  authorityPosture: 'R3D1_CORE_EXECUTION_PASS_RECEIPT_PENDING_R3D2_NOT_STARTED_RUN_8E_FAIL_OPEN',
+  authoritySource: ['R3C_PASS_CLOSED_PERSISTENT_RENDERER', 'R3D_BOUNDED_SUBCHECKPOINT_DECOMPOSITION', 'R3D1_GITHUB_ACTIONS_STATIC_VALIDATION'],
+  authorityScope: ['PRESERVE_DIAGNOSTIC_DIRECTORY', 'PRESERVE_HTML_HOST', 'PRESERVE_NONEXECUTING_PLACEHOLDERS', 'PRESERVE_PATH_RESOLUTION_EVIDENCE'],
   authorityLimitations: ['NO_INTERACTION', 'NO_RENDERER_EXECUTION', 'NO_PUBLIC_ROUTE', 'NO_DEPLOYMENT', 'NO_R3D2', 'NO_RUN_8E_PASS'],
   parentRelations: [], childRelations: [], peerRelations: [], upstreamBoundaries: [], downstreamBoundaries: [],
   cardinalRole: 'NONE', cardinalStatus: 'NONE', cardinalCompleteness: 'NOT_APPLICABLE',
@@ -99,11 +116,11 @@ export const H_EARTH_RUN_8E_R3D1_NODE = freeze({
   dependencyRelations: [],
   allowedMutationScope: 'R3D1_ELEVEN_PATH_BOUNDED_SCOPE_ONLY',
   prohibitedMutations: ['PUBLIC_ROUTE', 'PUBLIC_DIRECT_MANIPULATION', 'NAVIGATION_AUTHORITY', 'PERSISTENT_RENDERER', 'INTERACTION_BINDING', 'R3D2_OR_LATER'],
-  requiredValidations: ['STATIC_FILE_EXISTENCE', 'MODULE_IMPORT_RESOLUTION', 'STATIC_HTTP_RESOLUTION', 'FORBIDDEN_EXECUTION_TOKEN_SCAN', 'AUTOMATIC_REGISTRY_PREFLIGHT', 'EXACT_SCOPE'],
+  requiredValidations: ['STATIC_FILE_EXISTENCE', 'MODULE_IMPORT_RESOLUTION', 'STATIC_HTTP_RESOLUTION', 'FORBIDDEN_EXECUTION_TOKEN_SCAN', 'AUTOMATIC_REGISTRY_PREFLIGHT', 'EXACT_SCOPE', 'DURABLE_PASS_RECEIPT', 'FINAL_EXACT_HEAD_REVALIDATION'],
   stoppingBoundaries: ['STOP_BEFORE_POINTER_AND_TOUCH_INTAKE_R3D2'],
-  currentIdentityReferences: ['5c7a7eef489da94a230812eecc5e531e285b7cac'],
-  lifecycleStatus: 'EXECUTION_PENDING',
-  unresolvedFields: ['R3D1_WORKFLOW_RUN', 'R3D1_ARTIFACT', 'R3D1_PASS_RECEIPT']
+  currentIdentityReferences: ['5c7a7eef489da94a230812eecc5e531e285b7cac', '7cff5f1800c6e0743a44ac41ed501bf0c266dc61', '30294915207', '8664228635'],
+  lifecycleStatus: 'CORE_EXECUTION_PASS_RECEIPT_PENDING',
+  unresolvedFields: ['DURABLE_PASS_RECEIPT', 'FINAL_EXACT_HEAD', 'FINAL_EXACT_HEAD_WORKFLOW_RUN']
 });
 
 const pathIndex = new Map(H_EARTH_RUN_8E_R3D1_PATHS.map((repositoryPath) => [repositoryPath, {

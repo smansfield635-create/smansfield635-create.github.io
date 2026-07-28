3ccb698077bab9feda767f9cb38025b4d5b776aa
smansfield635-create <smansfield635@gmail.com>
2026-07-28 14:33:00 -0500

    Wire compact interaction acceptance into H-Earth runtime

--- showroom/globe/h-earth/functional-landscape/public-live-gpu-integration.run8e-r3e.receipt.js
+++ showroom/globe/h-earth/functional-landscape/public-live-gpu-integration.run8e-r3e.receipt.js
@@ -3,6 +3,7 @@ import '../diagnostic/renderer-startup-observer.v1.js';
 try {
   await import('./public-live-gpu-integration.run8e-r3e.js');
   window.H_EARTH_RENDERER_STARTUP_DIAGNOSTICS?.constructorReturned();
+  await import('../diagnostic/run8e-r3d/interaction-acceptance.run8e.js');
 } catch (error) {
   window.H_EARTH_RENDERER_STARTUP_DIAGNOSTICS?.fail('RENDERER_CONSTRUCTOR_RETURNED', error);
   throw error;

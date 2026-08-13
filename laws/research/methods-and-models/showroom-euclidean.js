221459d87983e63f9aded62269a90b33e459aa41
smansfield635-create <smansfield635@gmail.com>
2026-08-13 18:02:48 -0500

    Load rotational text assets directly

--- laws/research/methods-and-models/showroom-euclidean.js
+++ laws/research/methods-and-models/showroom-euclidean.js
@@ -1,7 +1,15 @@
 (() => {
   "use strict";
+
+  const contract = "METHODS_MODELS_ROTATIONAL_TEXT_INSTRUMENT_v1";
+  const stylesheet = document.createElement("link");
+  stylesheet.rel = "stylesheet";
+  stylesheet.href = "/laws/research/methods-and-models/rotational-text.css?v=METHODS_MODELS_ROTATIONAL_TEXT_INSTRUMENT_V1";
+  stylesheet.dataset.mmRotationalTextLoader = contract;
+
   const script = document.createElement("script");
   script.src = "/laws/research/methods-and-models/rotational-text.js?v=METHODS_MODELS_ROTATIONAL_TEXT_INSTRUMENT_V1";
-  script.dataset.mmRotationalTextLoader = "METHODS_MODELS_ROTATIONAL_TEXT_INSTRUMENT_v1";
-  document.head.append(script);
+  script.dataset.mmRotationalTextLoader = contract;
+
+  document.head.append(stylesheet, script);
 })();

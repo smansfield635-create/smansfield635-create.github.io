c111c670c9f6ee7514e1d7ec1f29321eef59f767
smansfield635-create <smansfield635@gmail.com>
2026-08-15 02:33:24 -0400

    Concentrate OW01 bay deformation without reducing maximum reach

--- h-earth-3d/terrain/h-earth.terrain-field.js
+++ h-earth-3d/terrain/h-earth.terrain-field.js
@@ -36,6 +36,8 @@ const bell = (value, center, radius) => {
   const retained = 1 - d * d;
   return retained * retained;
 };
+const concentratedBell = (value, center, radius, concentration) =>
+  Math.pow(bell(value, center, radius), concentration);
 
 export const H_EARTH_TERRAIN_FIELD_CONTRACT_ID = 'H_EARTH_CANONICAL_TERRAIN_FIELD_RUN_6B_v1';
 
@@ -54,6 +56,7 @@ export const H_EARTH_GRATITUDE_COASTAL_SYSTEM = deepFreeze({
     maximumInlandReach: 48,
     westernHeadlandX: 48,
     easternHeadlandX: 198,
+    realizationConcentration: 3,
     morphology: 'ASYMMETRIC_CURVED_INLAND_BAY_NOT_STRAIGHT_CUT'
   },
   harbor: {
@@ -113,10 +116,15 @@ export function getHEarthCanonicalShorelineZ(worldX) {
   const canonicalBackbone = coast.baselineZ
     + 7.5 * Math.sin(worldX / 58)
     + 2.75 * Math.sin((worldX + 31) / 19);
-  const bayCore = -bay.maximumInlandReach * bell(worldX, bay.centerX, bay.halfWidth);
-  const bayAsymmetry = -9.0 * bell(worldX, bay.centerX + 22, 44);
-  const westernHeadland = 10.5 * bell(worldX, bay.westernHeadlandX, 42);
-  const easternHeadland = 7.5 * bell(worldX, bay.easternHeadlandX, 46);
+  const bayCore = -bay.maximumInlandReach * concentratedBell(
+    worldX,
+    bay.centerX,
+    bay.halfWidth,
+    bay.realizationConcentration
+  );
+  const bayAsymmetry = -9.0 * concentratedBell(worldX, bay.centerX + 22, 44, 2);
+  const westernHeadland = 10.5 * concentratedBell(worldX, bay.westernHeadlandX, 42, 2);
+  const easternHeadland = 7.5 * concentratedBell(worldX, bay.easternHeadlandX, 46, 2);
   return canonicalBackbone + bayCore + bayAsymmetry + westernHeadland + easternHeadland;
 }
 

0b6161c446acbd0a5f3a3fdf3a2db58bf15ea9a5
smansfield635-create <smansfield635@gmail.com>
2026-08-16 00:01:39 -0500

    Repair Laws carousel live asset freshness

--- laws/index.mobile-background-tabs.js
+++ laws/index.mobile-background-tabs.js
@@ -13,7 +13,7 @@
   });
 
   load("/laws/index.mobile-background-tabs.core.js?v=LAWS_ROOT_ROLODEX_RESPONSIVE_CONTINUITY_V4_FROZEN", "data-laws-responsive-core")
-    .then(() => load("/laws/index.destination-carousel.js?v=LAWS_DESTINATION_CAROUSEL_RUNTIME_V2_20260815", "data-laws-destination-carousel-runtime"))
+    .then(() => load("/laws/index.destination-carousel.js?v=LAWS_DESTINATION_CAROUSEL_RUNTIME_V2_20260816B", "data-laws-destination-carousel-runtime"))
     .catch(error => {
       document.documentElement.dataset.lawsDestinationCarouselRuntime = "load-failed";
       console.error("Laws destination carousel bootstrap failed", error);

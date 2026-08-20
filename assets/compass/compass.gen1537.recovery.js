9d82a45578b7dcdbdd188aab06723552af8f55ee
smansfield635-create <smansfield635@gmail.com>
2026-08-20 13:45:54 -0400

    promote(h-earth): bind Compass to guarded experience successor

--- assets/compass/compass.gen1537.recovery.js
+++ assets/compass/compass.gen1537.recovery.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const PERFECTED_H_EARTH='https://raw.githack.com/smansfield635-create/smansfield635-create.github.io/04b320424370bd09cfb18b43343d4ace7127d1de/showroom/globe/h-earth/index.html';
+const PERFECTED_H_EARTH='https://raw.githack.com/smansfield635-create/smansfield635-create.github.io/53f6e1b7081925606d59ff6bcdad9dac3c39cdf8/showroom/globe/h-earth/index.html';
 const bindPerfectedHEarth=()=>{
   document.querySelectorAll('[data-compass-room][data-label="H-Earth"]').forEach(el=>{
     el.dataset.route=PERFECTED_H_EARTH;
@@ -34,7 +34,7 @@ const receipt=Object.freeze({
   mounted:false,
   retired:true,
   authoritative:false,
-  version:'gen1537-historical-artifact-retired-h-earth-experience-successor-23949-v1',
+  version:'gen1537-historical-artifact-retired-h-earth-experience-successor-23949-v2',
   instrumentMounted:false,
   lowerCarouselPreserved:true,
   clusterGuidanceMounted:false,

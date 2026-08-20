d29d2b8ec114669149f09904ebd269ac45c34c62
smansfield635-create <smansfield635@gmail.com>
2026-08-20 13:59:03 -0400

    H-Earth: separate loader and use one gentle ambience authority

--- showroom/globe/h-earth/functional-landscape/public-live-gpu-integration.run8e-r3e.receipt.js
+++ showroom/globe/h-earth/functional-landscape/public-live-gpu-integration.run8e-r3e.receipt.js
@@ -1,6 +1,6 @@
 /** H_EARTH_RENDERER_STARTUP_DIAGNOSTIC_RECEIPT_v1 integration wrapper */
 import '../diagnostic/renderer-startup-observer.v1.js';
-await import('../experience-layer.js');
+await import('../arrival-loader.js');
 await import('../environmental-audio-media.js');
 try {
   await import('./public-live-gpu-integration.run8e-r3e.js');

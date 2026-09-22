51d8c1a5e34b8232f5fb0baa67846cc7acb998e8
smansfield635-create <smansfield635@gmail.com>
2026-09-22 13:45:56 -0400

    Revision Run 8E integration import boundary

--- showroom/globe/h-earth/functional-landscape/public-live-gpu-integration.run8e-r3e.receipt.js
+++ showroom/globe/h-earth/functional-landscape/public-live-gpu-integration.run8e-r3e.receipt.js
@@ -2,7 +2,7 @@
 import '../diagnostic/renderer-startup-observer.v1.js';
 await import('../arrival-loader.js');
 try {
-  await import('./public-live-gpu-integration.run8e-r3e.js');
+  await import('./public-live-gpu-integration.run8e-r3e.js?v=run8e-cache-coherence-v1');
   window.H_EARTH_RENDERER_STARTUP_DIAGNOSTICS?.constructorReturned();
 } catch (error) {
   window.H_EARTH_RENDERER_STARTUP_DIAGNOSTICS?.fail('RENDERER_CONSTRUCTOR_RETURNED', error);

8edc26eb1a24fdddad05a205c26a18093b4e641d
smansfield635-create <smansfield635@gmail.com>
2026-09-23 09:42:14 -0500

    Restore protected Phase 2B canonical transport: gpu-upload-views.run8e-r2d.js

--- showroom/globe/h-earth/render/gpu-upload-views.run8e-r2d.js
+++ showroom/globe/h-earth/render/gpu-upload-views.run8e-r2d.js
@@ -204,7 +204,6 @@ export function createHEarthRun8ER2DCanonicalGPUUploadViews(
 
   return freezeRecord({
     positions: new Float32Array(rawViews.positions),
-    localAuthoringXZ: new Float32Array(rawViews.localAuthoringXZ),
     normals: canonicalNormals.view,
     baseColorsLinear: projectedWaterColors.view,
     materialParameters: canonicalMaterialParameters.view,
@@ -234,7 +233,6 @@ export function evaluateHEarthRun8ER2DCanonicalGPUUploadViews(views) {
   const issues = [];
   const expected = {
     positions: Float32Array,
-    localAuthoringXZ: Float32Array,
     normals: Float32Array,
     baseColorsLinear: Float32Array,
     materialParameters: Float32Array,
